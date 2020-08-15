0354b376bc2903eab9a82e5f51eb3837d20bd344
Sugam Gupta <sugam192@gmail.com>
2020-08-15 12:03:01 +0530

    sample store app

--- sample_store_app/app.py
+++ sample_store_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
@@ -15,18 +15,30 @@ stores = [
     }
 ]
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 # POST - used to receive data
 # GET  - used to send data back only
 
 # POST /store data: {name:}
 @app.route('/store',methods=['POST'])
 def create_store():
-    pass
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
 
 # GET /store/<string:name>
 @app.route('/store/<string:name>')  # 'http://127.0.0.1:5000/store/some_name'
 def get_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify(store)
+    return jsonify({'message': 'store not found'})
 
 
 # GET /store
@@ -37,10 +49,24 @@ def get_stores():
 # POST /store/<string:name>/item {name:, price:}
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(new_item)
+    return jsonify({'message': 'store not found'})
+
 
 # GET /store/<string:name>/item
 @app.route('/store/<string:name>/item')
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store['items']})
+    return jsonify({'message': 'store not found'})
+
 app.run(port=5000)
